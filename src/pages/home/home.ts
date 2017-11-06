69fc92c413d2f9981dffc08028cd631cbdbca313
Matt Neel <matt.neel@vallationinc.com>
2017-11-06 11:22:39 -0800

    Initial barcode scanning example.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 
+@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public barcode: BarcodeScanner
+  ) {}
+  results: any = null;
 
-  constructor(public navCtrl: NavController) {
-
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad HomePage");
   }
 
+  scan() {
+    this.barcode.scan().then(data => {
+      this.results = data;
+      console.dir(data);
+    });
+  }
+
+  clearResults() {
+    this.results = null;
+  }
 }
